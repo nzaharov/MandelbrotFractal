46c4ac4c08d93c480d622790b49e05a0ba3cc196
Nikolay Zaharov <nzaharov@x570.localdomain>
2020-06-14 23:54:05 +0300

    implementation

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 extern crate image;
 
 use image::RgbImage;
-
 use std::path::PathBuf;
+use std::sync::mpsc;
 use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::Instant;
@@ -24,7 +24,7 @@ struct Cli {
     #[structopt(short, long, default_value = "1")]
     threads: usize,
     #[structopt(short, long, default_value = "zad15.png")]
-    output: PathBuf, // TODO image format validation?
+    output: PathBuf,
     #[structopt(short = "q", long = "quiet")]
     is_quiet: bool,
 }
@@ -38,60 +38,69 @@ fn main() {
         threads,
         ..
     } = args;
-    let img = RgbImage::new(size.width, size.height);
+    let mut img = RgbImage::new(size.width, size.height);
 
     let now = Instant::now();
 
     let scale_x = (rect.a2 - rect.a1) / (size.width as f64 - 1.0);
     let scale_y = (rect.b2 - rect.b1) / (size.height as f64 - 1.0);
 
-    let band_heights = (0..size.height).collect::<Vec<u32>>();
     let chunk_size = match size.height as usize / threads / threads {
         0 => 1,
         res => res,
     };
-    let bands_iter = band_heights.chunks(chunk_size).map(|band| {
-        band.iter()
-            .map(|h| {
-                (0..size.width)
-                    .map(move |w| (*h, w))
-                    .collect::<Vec<(u32, u32)>>()
-            })
-            .flatten()
-            .collect::<Vec<(u32, u32)>>()
-    });
 
-    // let mut handles = vec![];
-    let img_arc = Arc::new(Mutex::new(img));
+    let band_heights: Vec<u32> = (0..size.height).collect::<Vec<u32>>();
+    let bands_iter = band_heights
+        .chunks(chunk_size)
+        .map(move |band| {
+            band.into_iter()
+                .map(|h| {
+                    (0..size.width)
+                        .map(move |w| (*h, w))
+                        .collect::<Vec<(u32, u32)>>()
+                })
+                .flatten()
+                .collect::<Vec<(u32, u32)>>()
+        })
+        .collect::<Vec<Vec<(u32, u32)>>>();
 
-    for (i, band) in bands_iter.enumerate() {
-        println!("{}", i);
+    let (tx, rx) = mpsc::channel::<(u32, u32, image::Rgb<u8>)>();
+
+    let tx_arc = Arc::new(tx);
+    let bands_arc = Arc::new(Mutex::new(bands_iter.into_iter()));
+    for _ in 0..threads {
+        let a1 = rect.a1;
+        let b1 = rect.b1;
+        let bands_clone = Arc::clone(&bands_arc);
+        let sender = mpsc::Sender::clone(&tx_arc);
+        thread::spawn(move || {
+            let bands_iter = &*bands_clone;
+            loop {
+                let band = bands_iter.lock().unwrap().next();
+                if band.is_none() {
+                    break;
+                }
+                for (y, x) in band.unwrap() {
+                    let re = x as f64 * scale_x + a1;
+                    let im = y as f64 * scale_y + b1;
+                    sender
+                        .send((x as u32, y as u32, mandelbrot(re, im)))
+                        .unwrap();
+                }
+            }
+            drop(sender);
+        });
     }
+    drop(tx_arc);
 
-    // for band in bands_iter {
-    //     let a1 = rect.a1;
-    //     let b1 = rect.b1;
-    //     let img_clone = Arc::clone(&img_arc);
-    //     let handle = thread::spawn(move || {
-    //         for (y, x) in band {
-    //             let re = x as f64 * scale_x + a1;
-    //             let im = y as f64 * scale_y + b1;
-    //             img_clone
-    //                 .lock()
-    //                 .unwrap()
-    //                 .put_pixel(x as u32, y as u32, mandelbrot(re, im));
-    //         }
-    //     });
-    //     handles.push(handle);
-    // }
-
-    // for handle in handles {
-    //     handle.join().unwrap();
-    // }
+    for pixel in rx {
+        img.put_pixel(pixel.0, pixel.1, pixel.2);
+    }
 
     println!("Image buffer filled: {}ms", now.elapsed().as_millis());
 
-    img_arc.lock().unwrap().save(file_name).unwrap();
+    img.save(file_name).unwrap();
 
     println!("Image write complete: {}ms", now.elapsed().as_millis());
 }
